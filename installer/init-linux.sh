3db6a954e60313111fbb87c4828f6a846dd6e8fe
Hamed Masafi <hamed.masafi@gmail.com>
2020-08-22 11:58:30 +0430

    3

--- installer/init-linux.sh
+++ installer/init-linux.sh
@@ -20,4 +20,6 @@ cp $DST/QtConfig.sh $DST_DATA
 
 ldd $SRC/QtConfig | grep -i qt  | egrep -o '/.*\.so.* ' | sed -e 's/\s//g' | tr \\n \\0 | xargs -0 -I{} cp "{}" $DST_DATA
 
-$BINARY_CREATOR -c $DST_PLATFORM/config.xml -p $DST_PLATFORM/packages $DST_PLATFORM/qtconfig-linux
+mkdir -p $DST_PLATFORM/output
+
+$BINARY_CREATOR -c $DST_PLATFORM/config.xml -p $DST_PLATFORM/packages $DST_PLATFORM/output/qtconfig-linux
