41a29a37a42225a4149794a419b92e76a54e4ef9
Hamed Masafi <hamed.masafi@gmail.com>
2020-08-22 11:42:25 +0430

    2

--- installer/init-linux.sh
+++ installer/init-linux.sh
@@ -4,9 +4,11 @@ QT_PATH=$1
 OUTPUT_NAME=$2
 SRC=$(pwd)
 PLATFORM=linux
+
 DST="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 DST_PLATFORM="$DST/$PLATFORM"
 DST_DATA="$DST_PLATFORM/packages/ir.tooska.confiqt/data/"
+BINARY_CREATOR=$(ls $QT_PATH/Tools/QtInstallerFramework/*/bin/binarycreator | head -1)
 
 mkdir $DST_PLATFORM
 cp $DST/template/* $DST_PLATFORM -R
@@ -18,4 +20,4 @@ cp $DST/QtConfig.sh $DST_DATA
 
 ldd $SRC/QtConfig | grep -i qt  | egrep -o '/.*\.so.* ' | sed -e 's/\s//g' | tr \\n \\0 | xargs -0 -I{} cp "{}" $DST_DATA
 
-$$QT_PATH/Tools/QtInstallerFramework/3.2/bin/binarycreator -c $DST_PLATFORM/config.xml -p $DST_PLATFORM/packages $DST_PLATFORM/qtconfig-linux
+$BINARY_CREATOR -c $DST_PLATFORM/config.xml -p $DST_PLATFORM/packages $DST_PLATFORM/qtconfig-linux
