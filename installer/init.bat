0faaf9b7ee1f326434359ca5b265b41638782745
Hamed Masafi <hamed.masafi@gmail.com>
2020-08-23 10:13:55 +0430

    win

--- installer/init.bat
+++ installer/init.bat
@@ -9,13 +9,20 @@ set arch=%3
 
 set pwd=%~dp0
 set out_path=%pwd%\output
-set data_path="%pwd%\packages\ir.tooska.confiqt\data\"
+set data_path=%pwd%\packages\ir.tooska.confiqt\data\
 
 rem create_paths
 mkdir %out_path%
 
 rem  copy files
-xcopy /y /q %pwd%\QtConfig.exe %data_path% 
+xcopy /y /q %pwd%\..\release\QtConfig.exe %data_path% 
+
+echo Copying files to data dir
+if %arch%==x86 (
+	%qt_path%\5.15.0\msvc2019\bin\windeployqt.exe --verbose 0 %data_path%
+) else (
+	%qt_path%\5.15.0\msvc2019_64\bin\windeployqt.exe --verbose 0 %data_path%
+)
 
 rem  make setup
 set binary_creator=%qt_path%\Tools\QtInstallerFramework\3.2\bin\binarycreator.exe
